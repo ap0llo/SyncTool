4bde33581a4553af3a6dcc1bcffc988a510b2294
Andreas Grünwald <andreasgruenwald@live.de>
2015-11-18 19:13:12 +0100

    Added SyncTool.Configuration project

---
+++ SyncTool.Configuration/main/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SyncTool.Configuration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SyncTool.Configuration")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c4dfbc8-1dd7-4ea3-9ef5-c06693560784")]
+
---
+++ SyncTool.Configuration/test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SyncTool.Configuration.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SyncTool.Configuration.Test")]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f0f54e2-c072-44c1-b723-1d7cd1996b1f")]
+
