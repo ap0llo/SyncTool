c26bb11aff24388f18ffa5a0954a0b41e4f744f9
Andreas Gruenwald <andreasgruenwald@live.de>
2015-11-21 20:13:42 +0100

    Implemented converter that applies a filter to a directory

---
+++ SyncTool.FileSystem/main/Filtering/FilteringConverter.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------------------------------------------
+//  Copyright (c) 2015, Andreas Grünwald
+//  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// -----------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SyncTool.FileSystem.Filtering
+{
+    public class FilteringConverter
+    {
+        readonly IFileSystemFilter m_Filter;
+
+        public FilteringConverter(IFileSystemFilter filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+            m_Filter = filter;
+        }
+
+
+
+        public IDirectory Convert(IDirectory directory) => Convert(null, directory);
+
+
+
+        IDirectory Convert(IDirectory parentDirectory, IDirectory toConvert)
+        {
+            var newDirectory = new Directory(null, toConvert.Name);
+
+            foreach (var subDirectory in toConvert.Directories)
+            {
+                if (m_Filter.Applies(subDirectory) == false)
+                {
+                    newDirectory.Add(d => Convert(d, subDirectory));                    
+                }
+            }
+
+            foreach (var file in toConvert.Files)
+            {
+                if (m_Filter.Applies(file) == false)
+                {
+                    newDirectory.Add(d => file.WithParent(d));
+                }
+            }
+
+            return newDirectory;
+        }
+
+    }
+}
---
+++ SyncTool.FileSystem/test/Filtering/FilteringConverterTest.cs
@@ -0,0 +1,52 @@
+﻿// // -----------------------------------------------------------------------------------------------------------
+// //  Copyright (c) 2015, Andreas Grünwald
+// //  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// // -----------------------------------------------------------------------------------------------------------
+
+using Moq;
+using Xunit;
+
+namespace SyncTool.FileSystem.Filtering
+{
+    public class FilteringConverterTest
+    {
+        const string s_File1 = "file1";
+        const string s_File2 = "file2";
+           
+
+        public FilteringConverterTest()
+        {
+            
+        }
+
+
+        [Fact(DisplayName = nameof(FilteringConverter) + "Convert() removes files from result")]
+        public void Convert_removes_files_from_result()
+        {            
+            var directory = new Directory(null, "root")
+            {
+                root => new File(root, s_File1),
+                root => new File(root, s_File2)
+            };
+
+            var filter = new Mock<IFileSystemFilter>(MockBehavior.Strict);
+            filter.Setup(f => f.Applies(It.Is<IFileSystemItem>(item => item.Name == s_File1))).Returns(true);
+            filter.Setup(f => f.Applies(It.Is<IFileSystemItem>(item => item.Name == s_File2))).Returns(false);
+
+            var converter = new FilteringConverter(filter.Object);
+
+            var filteredDirectory = converter.Convert(directory);
+
+            Assert.Single(filteredDirectory.Files);
+            Assert.Empty(filteredDirectory.Directories);
+
+            Assert.True(directory.FileExists(s_File2));            
+        }
+
+
+
+
+        
+
+    }
+}
