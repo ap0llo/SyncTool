06fb98eff44cb16d7ecae7605c8dc9ab300bca55
Andreas Grünwald <andreasgruenwald@live.de>
2015-11-30 21:49:20 +0100

    Revert "Temporarily deactivated all configuration tests to fix build error"
    This reverts commit 35fdbea71c5ecff3473e5c4a3b2c1a3903c0a278.

--- SyncTool.Configuration/test/JsonSyncFolderReaderTest.cs
+++ SyncTool.Configuration/test/JsonSyncFolderReaderTest.cs
@@ -1,69 +1,69 @@
-﻿//// -----------------------------------------------------------------------------------------------------------
-////  Copyright (c) 2015, Andreas Grünwald
-////  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
-//// -----------------------------------------------------------------------------------------------------------
-//
-//using System;
-//using System.IO;
-//using System.Linq;
-//using SyncTool.Configuration.Model;
-//using SyncTool.Configuration.Reader;
-//using SyncTool.Utilities;
-//using Xunit;
-//
-//namespace SyncTool.Configuration.Test
-//{
-//    public class JsonSyncFolderReaderTest
-//    {
-//
-//        readonly JsonSyncFolderReader m_Instance = new JsonSyncFolderReader();
-//
-//
-//        [Fact(DisplayName = nameof(JsonSyncFolderReader) + ".ReadSyncFolder() successfully reads valid json stream")]
-//        public void ReadSyncFolder_successfully_reads_valid_json_stream()
-//        {
-//
-//
-//            var json1 = @"
-//                            { 
-//                                ""name"" : ""foo"", 
-//                                ""path"" : ""bar""                                 
-//                            }
-//                         ";
-//
-//            var json2 = @"
-//                            {   
-//                                ""name"" : ""foo2"", 
-//                                ""path"" : ""bar2"",                                 
-//                                ""readFilter"" : { ""type"" : ""microscopeQuery"" , ""query"" : ""test"" }
-//                            }                
-//                        ";
-//
-//
-//            SyncFolder syncFolder1;
-//            SyncFolder syncFolder2;
-//            using (var stream = json1.ToStream())
-//            {
-//                syncFolder1 = m_Instance.ReadSyncFolder(stream);
-//            }
-//            using (var stream = json2.ToStream())
-//            {
-//                syncFolder2 = m_Instance.ReadSyncFolder(stream);
-//            }
-//
-//
-//
-//            Assert.Equal("foo", syncFolder1.Name);
-//            Assert.Equal("bar", syncFolder1.Path);
-//            Assert.Null(syncFolder1.ReadFilter);
-//
-//            Assert.Equal("foo2", syncFolder2.Name);
-//            Assert.Equal("bar2", syncFolder2.Path);
-//            Assert.NotNull(syncFolder2.ReadFilter);
-//            Assert.Equal(FileSystemFilterType.MicroscopeQuery, syncFolder2.ReadFilter.Type);
-//            Assert.Equal("test", syncFolder2.ReadFilter.Query);
-//
-//        }
-//
-//    }
-//}
+﻿// -----------------------------------------------------------------------------------------------------------
+//  Copyright (c) 2015, Andreas Grünwald
+//  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// -----------------------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Linq;
+using SyncTool.Configuration.Model;
+using SyncTool.Configuration.Reader;
+using SyncTool.Utilities;
+using Xunit;
+
+namespace SyncTool.Configuration.Test
+{
+    public class JsonSyncFolderReaderTest
+    {
+
+        readonly JsonSyncFolderReader m_Instance = new JsonSyncFolderReader();
+
+
+        [Fact(DisplayName = nameof(JsonSyncFolderReader) + ".ReadSyncFolder() successfully reads valid json stream")]
+        public void ReadSyncFolder_successfully_reads_valid_json_stream()
+        {
+
+
+            var json1 = @"
+                            { 
+                                ""name"" : ""foo"", 
+                                ""path"" : ""bar""                                 
+                            }
+                         ";
+
+            var json2 = @"
+                            {   
+                                ""name"" : ""foo2"", 
+                                ""path"" : ""bar2"",                                 
+                                ""readFilter"" : { ""type"" : ""microscopeQuery"" , ""query"" : ""test"" }
+                            }                
+                        ";
+
+
+            SyncFolder syncFolder1;
+            SyncFolder syncFolder2;
+            using (var stream = json1.ToStream())
+            {
+                syncFolder1 = m_Instance.ReadSyncFolder(stream);
+            }
+            using (var stream = json2.ToStream())
+            {
+                syncFolder2 = m_Instance.ReadSyncFolder(stream);
+            }
+
+
+
+            Assert.Equal("foo", syncFolder1.Name);
+            Assert.Equal("bar", syncFolder1.Path);
+            Assert.Null(syncFolder1.ReadFilter);
+
+            Assert.Equal("foo2", syncFolder2.Name);
+            Assert.Equal("bar2", syncFolder2.Path);
+            Assert.NotNull(syncFolder2.ReadFilter);
+            Assert.Equal(FileSystemFilterType.MicroscopeQuery, syncFolder2.ReadFilter.Type);
+            Assert.Equal("test", syncFolder2.ReadFilter.Query);
+
+        }
+
+    }
+}
