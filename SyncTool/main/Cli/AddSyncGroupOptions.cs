9e34cbf2aa66b24db4c0f04779181b9b9db23617
Andreas Grünwald <andreasgruenwald@live.de>
2015-12-03 16:40:55 +0100

    STarted adding a commandline interface to SyncTool.exe

---
+++ SyncTool/main/Cli/AddSyncGroupOptions.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------------------------------------------
+//  Copyright (c) 2015, Andreas Grünwald
+//  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// -----------------------------------------------------------------------------------------------------------
+
+using CommandLine;
+
+namespace SyncTool.Cli
+{
+    [Verb("Add-SyncGroup")]
+    public class AddSyncGroupOptions
+    {
+        [Option(Required = true)]
+        public string Name { get; set; }
+
+        [Option(Required = true)]
+        public string LocalPath { get; set; }
+
+        [Option(Required = false)]
+        public string RemotePath { get; set; }
+    }
+}
---
+++ SyncTool/main/Cli/GetSyncGroupOptions.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------------------------------------------
+//  Copyright (c) 2015, Andreas Grünwald
+//  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// -----------------------------------------------------------------------------------------------------------
+
+using CommandLine;
+
+namespace SyncTool.Cli
+{
+    [Verb("Get-SyncGroup")]
+    public class GetSyncGroupOptions
+    {
+
+        [Option(Required = false)]
+        public string Name { get; set; }
+        
+    }
+}
