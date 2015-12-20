84604d9302788c79f08e8d614e7894e9e01ead57
Andreas Gruenwald <andreasgruenwald@live.de>
2015-12-20 14:00:47 +0100

    Added Assembly "SyncTool.Synchronization"

---
+++ SyncTool.Synchronization/main/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SyncTool.Synchronization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SyncTool.Synchronization")]
+
+
+[assembly: InternalsVisibleTo("SyncTool.Synchronization.Test")]
