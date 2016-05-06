81c14c30ee4eceed75c1be230989a072177f16af
Andreas Gruenwald <andreasgruenwald@live.de>
2016-05-06 19:56:40 +0200

    Removed FileSystem.Filtering

--- SyncTool.Git/test/Configuration/GitBasedConfigurationServiceTest.cs
+++ SyncTool.Git/test/Configuration/GitBasedConfigurationServiceTest.cs
@@ -2,6 +2,7 @@
 //  Copyright (c) 2015-2016, Andreas Grünwald
 //  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
 // -----------------------------------------------------------------------------------------------------------
+
 using System;
 using System.Linq;
 using LibGit2Sharp;
