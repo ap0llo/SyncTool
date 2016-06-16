67c99184198a55401654044b2db5a9dd0ca785bf
Andreas Gruenwald <andreasgruenwald@live.de>
2016-06-16 20:10:58 +0200

    Added HistorySnapshotId and HistorySnapshotIdColleciton

---
+++ SyncTool.Synchronization/main/Synchronization/State/HistorySnapshotIdCollection.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------------------------------------------
+//  Copyright (c) 2016, Andreas Grünwald
+//  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// -----------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTool.Synchronization.State
+{
+    public sealed class HistorySnapshotIdCollection
+    {
+        readonly IDictionary<string, HistorySnapshotId> m_SnapshotIds;
+
+        public IEnumerable<HistorySnapshotId> SnapshotIds => m_SnapshotIds.Values;
+
+
+        public HistorySnapshotIdCollection(IEnumerable<HistorySnapshotId> snapshotIds)
+        {
+            if (snapshotIds == null)
+                throw new ArgumentNullException(nameof(snapshotIds));
+
+            m_SnapshotIds = snapshotIds.ToDictionary(id => id.HistoryName, StringComparer.InvariantCultureIgnoreCase);
+        }
+
+
+        public string GetSnapshotId(string historyName) => m_SnapshotIds[historyName].SnapshotId;
+        
+
+    }
+}
