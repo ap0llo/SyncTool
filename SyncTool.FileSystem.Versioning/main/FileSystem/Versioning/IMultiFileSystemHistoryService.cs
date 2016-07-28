ad0c8007e4a2f83cb3a177c0dec759b628e0f48b
Andreas Gruenwald <andreasgruenwald@live.de>
2016-07-28 17:37:32 +0200

    Added inline documentation for IMultiFileSystemHistoryService

--- SyncTool.FileSystem.Versioning/main/FileSystem/Versioning/IMultiFileSystemHistoryService.cs
+++ SyncTool.FileSystem.Versioning/main/FileSystem/Versioning/IMultiFileSystemHistoryService.cs
@@ -3,6 +3,7 @@
 //  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
 // -----------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using SyncTool.Common;
 
@@ -10,20 +11,55 @@ namespace SyncTool.FileSystem.Versioning
 {
     public interface IMultiFileSystemHistoryService : IService
     {         
+        /// <summary>
+        /// Gets the lastest snapshot or null, if no snapshot was created yet
+        /// </summary>
         IMultiFileSystemSnapshot LatestSnapshot { get; }
     
+        /// <summary>
+        /// Enumerates all snapshots
+        /// </summary>
         IEnumerable<IMultiFileSystemSnapshot> Snapshots { get; }
 
+        /// <summary>
+        /// Gets the snapshot with the specified id
+        /// </summary>
+        /// <param name="id">The id of the snapshot to retrieve</param>
+        /// <exception cref="ArgumentNullException">Thrown if the specified id is null, empty or whitespace</exception>
+        /// <exception cref="SnapshotNotFoundException">Thrown if the specified snapshot was not found</exception>
         IMultiFileSystemSnapshot this[string id] { get; }
 
+        /// <summary>
+        /// Saves the latest snapshots of the underlying file system histories
+        /// </summary>
         IMultiFileSystemSnapshot CreateSnapshot();
 
-        string[] GetChangedFiles(string to);
+        /// <summary>
+        /// Gets the paths of all changed files from the initial snapshot up to the specified snapshot
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if the specified id is null, empty or whitespace</exception>
+        /// <exception cref="SnapshotNotFoundException">Thrown if the specified snapshot was not found</exception>
+        string[] GetChangedFiles(string toId);
 
-        string[] GetChangedFiles(string from, string to);
+        /// <summary>
+        /// Gets the paths of all changed files in the specified range
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if one of the specified ids is null, empty or whitespace</exception>
+        /// <exception cref="SnapshotNotFoundException">Thrown if on of the specified snapshots was not found</exception>
+        string[] GetChangedFiles(string fromId, string toId);
 
+        /// <summary>
+        /// Gets the changes between the initial and the specified snapshot
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if the specified id is null, empty or whitespace</exception>
+        /// <exception cref="SnapshotNotFoundException">Thrown if the specified snapshot was not found</exception>
         IMultiFileSystemDiff GetChanges(string toId, string[] pathFilter = null);
 
+        /// <summary>
+        /// Gets the changes in the specified range
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if one of the specified ids is null, empty or whitespace</exception>
+        /// <exception cref="SnapshotNotFoundException">Thrown if on of the specified snapshots was not found</exception>
         IMultiFileSystemDiff GetChanges(string fromId, string toId, string[] pathFilter = null);
     }
 }
