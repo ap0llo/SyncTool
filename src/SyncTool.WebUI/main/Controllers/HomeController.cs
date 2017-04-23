9179170bad0d4d6ca36768d6a21f8c7e0276315e
Andreas Grünwald <andreasgruenwald@live.de>
2017-04-23 22:27:43 +0200

    Add (mostly empty) Asp.Net project for WebUI

---
+++ src/SyncTool.WebUI/main/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SyncTool.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "SyncTool WebUI";
+
+            return View();
+        }
+
+    
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
