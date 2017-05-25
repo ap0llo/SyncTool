4d048a51d0752d1a9dc5233f0c038ae9a50252a1
Andreas Grünwald <andreasgruenwald@live.de>
2017-05-25 21:52:52 +0200

    Add GroupStorage class

---
+++ src/SyncTool.Common/main/Common/GroupStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTool.Common
+{
+    public sealed class GroupStorage
+    {
+
+        public string Path { get; }
+
+
+        public GroupStorage(string path)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+                throw new ArgumentException("Value must not be empty", nameof(path));
+
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException($"Directory '{path}' does not exist");
+
+            Path = System.IO.Path.GetFullPath(path);
+        }
+    }
+}
