4af1c0af74a2bd5c89d5e2b19393eaa3f19bbfd6
Andreas Grünwald <andreasgruenwald@live.de>
2017-05-26 19:15:50 +0200

    Add TemporaryDirectory

---
+++ src/SyncTool.Utilities/main/TemporaryDirectory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTool.Utilities
+{
+    public sealed class TemporaryDirectory : IDisposable
+    {
+        public string FullName { get; }
+
+
+        public TemporaryDirectory()
+        {
+            FullName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(FullName);
+        }
+
+
+        public void Dispose() => DirectoryHelper.DeleteRecursively(FullName);
+
+
+        public static implicit operator string(TemporaryDirectory instance) => instance?.FullName;
+    }
+}
