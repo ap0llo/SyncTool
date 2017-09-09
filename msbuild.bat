1012e555e39fcc2d1d6da67327af3a40fa30a6d5
Andreas Grünwald <andreasgruenwald@live.de>
2017-09-09 17:18:33 +0200

    Remove vswhere.exe and use the vswhere executable installed by visual studio

--- msbuild.bat
+++ msbuild.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "usebackq tokens=*" %%i in (`tools\vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
   set InstallDir=%%i
 )
 
