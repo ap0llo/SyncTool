e6423ca5d5858b0bf79c243b81295ad004570106
Andreas Grünwald <andreasgruenwald@live.de>
2017-09-09 15:18:42 +0200

    Ensure database uri loaded from database does not contain a database name

--- src/SyncTool.Sql/testhelpers/SqlTestBase.cs
+++ src/SyncTool.Sql/testhelpers/SqlTestBase.cs
@@ -13,26 +13,32 @@ namespace SyncTool.Sql.TestHelpers
         
 
         public SqlTestBase()
-        {            
+        {      
+            // load database uri from environment variables      
             var mysqlUri = Environment.GetEnvironmentVariable("SYNCTOOL_TEST_MYSQLURI");
 
-            m_ConnectionString = new Uri(mysqlUri).ToMySqlConnectionString();
+            // get connection string without database name
+            var connectionStringBuilder = new Uri(mysqlUri).ToMySqlConnectionStringBuilder();
+            connectionStringBuilder.Database = null;
+            m_ConnectionString = connectionStringBuilder.ConnectionString;            
+
+            // create database to run tests against
             using (var connection = new MySqlConnection(m_ConnectionString))
             {
                 connection.Open();
                 connection.ExecuteNonQuery($"CREATE DATABASE {m_DatabaseName} ;");
             }
 
-            var connectionStringBuilder = new MySqlConnectionStringBuilder(m_ConnectionString)
-            {
-                Database = m_DatabaseName
-            };            
+            // create Database instance that will be used by tests
+            // this time inlcude the database name in the connection string
+            connectionStringBuilder.Database = m_DatabaseName;
             Database = new MySqlDatabase(connectionStringBuilder.ConnectionString);
         }
 
                        
         public void Dispose()
         {
+            // delete test database
             using (var connection = new MySqlConnection(m_ConnectionString))
             {
                 connection.Open();
