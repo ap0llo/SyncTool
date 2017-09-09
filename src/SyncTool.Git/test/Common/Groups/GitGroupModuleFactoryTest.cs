1021f1d4036e779385a68073237100a0f4875f87
Andreas Grünwald <andreasgruenwald@live.de>
2017-09-10 00:37:08 +0200

    Add unit tests for implementations of IGroupModuleFactory.IsAddressSupported()

---
+++ src/SyncTool.Git/test/Common/Groups/GitGroupModuleFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿using SyncTool.Git.Common.Groups;
+using Xunit;
+
+namespace SyncTool.Git.Test.Common.Groups
+{
+    public class GitGroupModuleFactoryTest
+    {
+        [Theory]
+        [InlineData(@"https://www.example.com", true)]
+        [InlineData(@"http://www.example.com", true)]
+        [InlineData(@"C:\MyRepository", true)]
+        [InlineData(@"\\server\Repository", true)]
+        [InlineData(@"synctool-mysql://server/database", false)]
+        public void IsAddressSupported_returns_expected_value(string address, bool expectedvalue)
+        {
+            var instance = new GitGroupModuleFactory();
+            Assert.Equal(expectedvalue, instance.IsAddressSupported(address));
+        }
+    }
+}
---
+++ src/SyncTool.Sql/test/SqlGroupModuleFactoryTest.cs
@@ -0,0 +1,21 @@
+﻿using SyncTool.Sql.DI;
+using Xunit;
+
+namespace SyncTool.Sql.Test
+{
+    public class SqlGroupModuleFactoryTest
+    {
+        [Theory]
+        [InlineData(@"https://someUrl", false)]       
+        [InlineData(@"mysql://someUrl", false)]       
+        [InlineData(@"http://someUrl", false)]       
+        [InlineData(@"\\somepath", false)]       
+        [InlineData(@"C:\some\path", false)]       
+        [InlineData(@"synctool-mysql://someUrl", true)]       
+        public void IsAddressSupported_returns_expected_value(string address, bool expectedValue)
+        {
+            var instance = new SqlGroupModuleFactory();
+            Assert.Equal(expectedValue, instance.IsAddressSupported(address));
+        }
+    }
+}
