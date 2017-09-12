b79cb68f21a5894f41198393ad051d7232c22530
Andreas Grünwald <andreasgruenwald@live.de>
2017-09-12 18:22:15 +0200

    Compare environment variable case-invariant

--- msbuild.bat
+++ msbuild.bat
@@ -5,7 +5,7 @@ for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\Microsoft Visual Studio
 )
 set MSBuildPath="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
 
-if "%APPVEYOR%" == "True" ( 
+if /I "%APPVEYOR%" == "True" ( 
     SET LOGGER=/logger:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll
 ) else (
     SET LOGGER=
